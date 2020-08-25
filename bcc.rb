658e37c5b6ea22272a0c0be4bd036ab52b136742
Sam Dexter <sdexter91@gmail.com>
2020-08-25 13:54:42 +0200

    final version bingo card

--- bcc.rb
+++ bcc.rb
@@ -33,6 +33,7 @@ BINGO
 puts bingo_card
 
 header = ["B", "I", "N", "G", "O"]
+n_col.insert(2, "F")
 
 Prawn::Document.generate("bingo.pdf") do
   define_grid(columns: 5, rows: 6)
@@ -42,5 +43,40 @@ Prawn::Document.generate("bingo.pdf") do
 	  stroke_bounds
 	  text char, align: :center, valign: :center, size: 50, style: :bold
 	end
+  end
+
+  b_col.each_with_index do |num, i|
+  	grid((i + 1), 0).bounding_box do
+  		stroke_bounds
+  		text num.to_s, align: :center, valign: :center, size: 50
+  	end
+  end
+
+  i_col.each_with_index do |num, i|
+  	grid((i + 1), 1).bounding_box do
+  		stroke_bounds
+  		text num.to_s, align: :center, valign: :center, size: 50
+  	end
+  end
+
+  n_col.each_with_index do |num, i|
+  	grid((i + 1), 2).bounding_box do
+  		stroke_bounds
+  		text num.to_s, align: :center, valign: :center, size: 50
+  	end
+  end
+
+  g_col.each_with_index do |num, i|
+  	grid((i + 1), 3).bounding_box do
+  		stroke_bounds
+  		text num.to_s, align: :center, valign: :center, size: 50
+  	end
+  end
+
+  o_col.each_with_index do |num, i|
+  	grid((i + 1), 4).bounding_box do
+  		stroke_bounds
+  		text num.to_s, align: :center, valign: :center, size: 50
+  	end
   end
 end
